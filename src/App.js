4efd25f9fb2c3202a88e24e909edd79617452317
DEEPAK-MSV <deepak@getpulse.in>
2023-03-20 17:29:38 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Cats from './Ani/Cats';
 import Dogs from './Ani/Dogs';
-import Homepage from './pages/Homepage';
+import HomePage from './pages/Homepage';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={Homepage}/>
-        <Route path='/Dogs' element={Dogs}/>
-        <Route path='/cats' element={Cats}/>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/Dogs' element={<Dogs/>}/>
+        <Route path='/cats' element={<Cats/>}/>
       </Routes>
     </BrowserRouter>
   );
